2d97856bb6e85926fda22adfa6c581450a05e3b5
Lenivaya <danylo.osipchuk@proton.me>
2025-03-06 01:11:11 +0200

    fix(ci): fix test ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     coverage: {
@@ -17,6 +16,7 @@ export default defineConfig({
         'next-env.d.ts',
         'vitest.config.ts'
       ],
+      reporter: ['html', 'json-summary', 'json', 'text'],
       provider: 'v8',
       reportOnFailure: true,
       thresholds: {
