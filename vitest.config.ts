e74f05bff58235973f17e87dc0b68c76aaf5f42b
Lenivaya <danylo.osipchuk@proton.me>
2025-03-06 01:08:23 +0200

    ci: add ci for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,26 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      enabled: true,
+      exclude: [
+        'node_modules',
+        'dist',
+        'next.config.ts',
+        'postcss.config.mjs',
+        'eslint.config.mjs',
+        'next-env.d.ts',
+        'vitest.config.ts'
+      ],
+      provider: 'v8',
+      reportOnFailure: true,
+      thresholds: {
+        branches: 40,
+        functions: 40,
+        lines: 40,
+        statements: 40
+      }
+    }
   }
 })
