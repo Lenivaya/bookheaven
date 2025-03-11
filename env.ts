2e7aea345a4915d719dc40371612079379ecdfc7
Lenivaya <danylo.osipchuk@proton.me>
2025-03-11 12:52:10 +0200

    feat(stripe): add simle webhooks handling

--- env.ts
+++ env.ts
@@ -6,7 +6,8 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string().min(1),
-    STRIPE_SECRET_KEY: z.string().min(1)
+    STRIPE_SECRET_KEY: z.string().min(1),
+    STRIPE_WEBHOOK_SECRET: z.string().min(1)
   },
   client: {},
   shared: {
