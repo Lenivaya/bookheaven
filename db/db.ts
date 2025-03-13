de2a92a37266c985ae97bddfbc7e8743ec6cd17f
Lenivaya <danylo.osipchuk@proton.me>
2025-03-14 01:28:21 +0200

    feat(db): use Neon Serverless for production database
    The changes made in this commit are:
    
    1. Replaced the usage of `drizzle-orm/neon-http` with `drizzle-orm/neon-serverless` for the production database connection.
    2. Configured the `neonConfig.webSocketConstructor` to use the `ws` module.
    3. Removed the commented-out code for the `drizzleNeonPostgres` connection.
    4. Updated the production database connection to use the `NeonServerLessPool` from the `@neondatabase/serverless` package.
    
    These changes were made to use the Neon Serverless database for the production environment, which provides a more scalable and managed database solution compared to the previous Neon HTTP-based approach.

--- db/db.ts
+++ db/db.ts
@@ -1,17 +1,30 @@
-import { drizzle as drizzleNeonPostgres } from 'drizzle-orm/neon-http'
+// import { drizzle as drizzleNeonPostgres } from 'drizzle-orm/neon-http'
+import { drizzle as drizzleNeonServerless } from 'drizzle-orm/neon-serverless'
 import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
 import { env } from '@/env'
 import { Pool } from 'pg'
 import * as schema from './schema'
-import { neon } from '@neondatabase/serverless'
+import {
+  Pool as NeonServerLessPool,
+  neonConfig
+} from '@neondatabase/serverless'
+import ws from 'ws'
 
 const isProduction = env.NODE_ENV === 'production'
+neonConfig.webSocketConstructor = ws
 
+// ? drizzleNeonPostgres(neon(env.DATABASE_URL), {
+//     schema,
+//     logger: !isProduction
+//   })
 export const db = isProduction
-  ? drizzleNeonPostgres(neon(env.DATABASE_URL), {
-      schema,
-      logger: !isProduction
-    })
+  ? drizzleNeonServerless(
+      new NeonServerLessPool({ connectionString: env.DATABASE_URL }),
+      {
+        schema,
+        logger: !isProduction
+      }
+    )
   : drizzlePostgres(
       new Pool({
         connectionString: env.DATABASE_URL
